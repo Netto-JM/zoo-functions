fc0c004f03d2460fce0d0ccbecc8f4a470b9c897
NettoJM <nettojm86@gmail.com>
2022-09-29 20:11:21 -0300

    getEmployeeByName completed

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,11 @@
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  const findEmployeeByName = (employeeInfo) => (
+    [employeeInfo.firstName, employeeInfo.lastName].includes(employeeName)
+  );
+  const employeeInfo = data.employees.find(findEmployeeByName) || {};
+  return employeeInfo;
 }
 
 module.exports = getEmployeeByName;
