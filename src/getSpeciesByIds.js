edc77b778c147ef5e8f4e5f27b3540f9d813ef45
NettoJM <nettojm86@gmail.com>
2022-09-29 18:17:39 -0300

    returning empty array when no ids is passed

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,25 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+/* Faça com que a função getSpeciesByIds possa receber vários parâmetros;
+
+Retorne um array vazio se a função não receber um id;
+
+Retorne as seguintes informações do arquivo data:
+
+Se a função receber apenas um id, retorne a espécie do animal referente a este id;
+
+Se a função receber vários ids, retorne todas as espécies referente a esses ids.
+
+O que será testado:
+
+A função getSpeciesByIds, caso não receba nenhum parâmetro, deve retornar um array vazio;
+
+A função getSpeciesByIds, caso receba como parâmetro um único ID, deve retornar um array com a espécie referente a esse ID;
+
+A função getSpeciesByIds, caso receba mais de um ID, deve retornar um array com as espécies referentes aos IDs. */
+
+function getSpeciesByIds(...ids) {
+  if (ids.length === 0) return [];
 }
 
 module.exports = getSpeciesByIds;
