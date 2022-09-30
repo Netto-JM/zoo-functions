3ae561136b61a9ff8f2b4652031c5da5e473a5d2
NettoJM <nettojm86@gmail.com>
2022-09-30 17:40:28 -0300

    countAnimals completed

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,16 @@
-const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  const filteredSpecies = species.filter((zooAnml) => !(animal) || zooAnml.name === animal.specie);
+  const getAnimalsCount = (acc, curr) => {
+    const genderFilter = (resident) => !(animal && animal.sex) || animal.sex === resident.sex;
+    const filteredResidents = curr.residents.filter(genderFilter);
+    acc[curr.name] = filteredResidents.length;
+    return acc;
+  };
+  const animalsCount = filteredSpecies.reduce(getAnimalsCount, {});
+  if (animal) return Object.values(animalsCount)[0];
+  return animalsCount;
 }
 
 module.exports = countAnimals;
