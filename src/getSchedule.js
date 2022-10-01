bd57576834b22a59509e5504da933343aa096c19
NettoJM <nettojm86@gmail.com>
2022-10-01 03:24:51 -0300

    getSchedule completed with many helper functions

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -4,7 +4,41 @@ const getAnimalAvailability = (animal) => (
   species.find((zooAnimal) => animal === zooAnimal.name).availability
 );
 
+const getFormattedOfficeHour = ({ open, close }) => `Open from ${open}am until ${close}pm`;
+
+const getExibitionByDay = (day) => {
+  const filteredSpeciesByDay = species.filter((zooAnimal) => zooAnimal.availability.includes(day));
+  const exibitedAnimals = filteredSpeciesByDay.map((zooAnimal) => zooAnimal.name);
+  return exibitedAnimals;
+};
+
+const mondayAlert = { exhibition: 'The zoo will be closed!', officeHour: 'CLOSED' };
+
+const getAllSchedule = () => {
+  const allSchedule = {};
+  Object.entries(hours).forEach((day) => {
+    const officeHour = getFormattedOfficeHour(day[1]);
+    const exhibition = getExibitionByDay(day[0]);
+    allSchedule[day[0]] = { officeHour, exhibition };
+  });
+  allSchedule.Monday = mondayAlert;
+  return allSchedule;
+};
+
+const getDaySchedule = (day) => {
+  if (day === 'Monday') return { Monday: mondayAlert };
+  const officeHour = getFormattedOfficeHour(hours[day]);
+  const exhibition = getExibitionByDay(day);
+  return { [day]: { officeHour, exhibition } };
+};
+
+const isADay = (argument) => Object.keys(hours).includes(argument);
+const isAnAnimal = (argument) => species.map((zooAnimal) => zooAnimal.name).includes(argument);
+const isValidArgument = (argument) => isADay(argument) || isAnAnimal(argument);
+
 function getSchedule(scheduleTarget) {
+  if (!isValidArgument(scheduleTarget)) return getAllSchedule();
+  if (isADay(scheduleTarget)) return getDaySchedule(scheduleTarget);
   return getAnimalAvailability(scheduleTarget);
 }
 
