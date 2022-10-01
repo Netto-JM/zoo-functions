6dd46223864227ffb8a991c2b6c8ca70ce54803b
NettoJM <nettojm86@gmail.com>
2022-10-01 16:07:18 -0300

    getOldestFromFirstSpecies completed

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,13 @@
-const data = require('../data/zoo_data');
+const { employees, species } = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const employee = employees.find((employeeInfo) => employeeInfo.id === id);
+  const firstSpeciesId = employee.responsibleFor[0];
+  const speciesResidents = species.find((animal) => animal.id === firstSpeciesId).residents;
+  const residentAges = speciesResidents.map((resident) => resident.age);
+  const maxAge = Math.max(...residentAges);
+  const oldestResident = speciesResidents.find((resident) => resident.age === maxAge);
+  return Object.values(oldestResident);
 }
 
 module.exports = getOldestFromFirstSpecies;
