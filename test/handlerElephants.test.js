9eae8b55550779abd0b93a68a3bc00346c2a4d04
NettoJM <nettojm86@gmail.com>
2022-10-08 01:47:22 -0300

    testing if handlerElephants function returns undefined when no parameters is passed

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,3 +1,7 @@
 const handlerElephants = require('../src/handlerElephants');
 
-describe('Testes da função HandlerElephants', () => {});
+describe('Testes da função HandlerElephants', () => {
+  test('Se nenhum porâmetro foi passado, retorna "undefined"', () => {
+    expect(handlerElephants()).toBeUndefined();
+  });
+});
